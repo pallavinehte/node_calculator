268e25103710c32c16c40e4960fcf3dcb180e296
Pallavi Nehte <pallavi.nehte@gmail.com>
2024-08-15 21:14:03 +0200

    changes Done

---
+++ __tests__/integration/integration.test.js
@@ -0,0 +1,24 @@
+import numberCruncher from '../../src/modules/calculation/numberCruncher';
+
+//const { numberCruncher } = require('../modules/numberCruncher');
+
+test('should add two numbers', () => {
+    const result = numberCruncher({ numOne: '5', numTwo: '3', operator: 'add' });
+    expect(result).toBe(8);
+});
+test('should subtract two numbers', () => {
+    const result = numberCruncher({ numOne: '5', numTwo: '3', operator: 'subtract' });
+    expect(result).toBe(2);
+});
+test('should multiply two numbers', () => {
+    const result = numberCruncher({ numOne: '5', numTwo: '3', operator: 'multiply' });
+    expect(result).toBe(15);
+});
+test('should divide two numbers', () => {
+    const result = numberCruncher({ numOne: '6', numTwo: '3', operator: 'divide' });
+    expect(result).toBe(2);
+});
+test('should percentage two numbers', () => {
+    const result = numberCruncher({ numOne: '50', numTwo: '100', operator: 'percentage' });
+    expect(result).toBe(50);
+}); 
---
+++ __tests__/unit/unit.test.js
@@ -0,0 +1,15 @@
+import percentage from '../../src/modules/calculation/util/percentage';
+import gcd from '../../src/modules/calculation/util/gcd';
+import exp from '../../src/modules/calculation/util/exp';
+
+//const { gcd, exp, percentage } = require('../modules/math');
+
+test('GCD of 12 and 8 is 4', () => {
+    expect(gcd(12, 8)).toBe(4);
+});
+test('exp(2, 3) is 8', () => {
+    expect(exp(2, 3)).toBe(8);
+});
+test('20 is 20% of 100', () => {
+    expect(percentage(20, 100)).toBe(20);
+});
